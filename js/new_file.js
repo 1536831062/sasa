67eb7281641ae28b5038a8f9441005cd5448c55c
1536831062 <15368310062@qq.com>
2018-02-27 15:19:34 +0800

    11

---
+++ js/new_file.js
@@ -0,0 +1,24 @@
+function validate_required(field,alerttxt)
+{
+with (field)
+  {
+  if (value==null||value=="")
+    {alert(alerttxt);return false}
+  else {return true}
+  }
+}
+
+function validate_form(thisform)
+{
+with (thisform)
+  {
+  if (validate_required(email,"Email must be filled out!")==false)
+    {email.focus();return false}
+  }
+}
+
+$(document).ready(function(){
+	$("form").submit(function(){
+		alert("确定要提交吗？");
+	});
+});
